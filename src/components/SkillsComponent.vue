6ffd0cfffe31fa9610232105759c9ad738eda9f0
ibrahiem96 <ibrahiem.mohammed96@gmail.com>
2021-11-08 01:39:58 +0000

    new experience and skills

--- src/components/SkillsComponent.vue
+++ src/components/SkillsComponent.vue
@@ -11,24 +11,34 @@ export default {
                         <cloud id='skills-chart'></cloud>
                     </div>
                     <div class="column">
-                        <h1>Programming</h1>
+                        <h1>Programming Languages</h1>
                         <programming id='skills-chart'></programming>
+                    </div>
+                </div>
+                <div class="columns">
+                    <div class="column">
+                        <h1>Security Tools</h1>
+                        <security id='skills-chart'></security>
+                    </div>
+                    <div class="column">
+                        <h1>Observability</h1>
+                        <observability id='skills-chart'></observability>
+                    </div>
+                </div>
+                <div class="columns">
+                    <div class="column">
+                        <h1>Infrastructure</h1>
+                        <infrastructure id='skills-chart'></infrastructure>
+                    </div>
+                    <div class="column">
+                        <h1>Container Tools / Service Mesh</h1>
+                        <containers id='skills-chart'></containers>
                     </div>
                 </div>
                 <div class="columns">
                     <div class="column">
                         <h1>CI/CD</h1>
                         <cicd id='skills-chart'></cicd>
-                    </div>
-                    <div class="column">
-                        <h1>Analytics</h1>
-                        <analytics id='skills-chart'></analytics>
-                    </div>
-                </div>
-                <div class="columns">
-                    <div class="column">
-                        <h1>Environments</h1>
-                        <environments id='skills-chart'></environments>
                     </div>
                     <div class="column">
                         <h1>Frameworks</h1>
@@ -46,15 +56,73 @@ const cloud = {
       return {
         datacollection: {
           //Data to be represented on x-axis
-          labels: ['Docker', 'AWS', 'Kubernetes', 'Azure'],
+          labels: ['AWS', 'GCP', 'Azure',],
           datasets: [
             {
-              backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f","#e8c3b9", "#b9e8e5"],
+              backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f"],
               pointBackgroundColor: 'white',
               borderWidth: 1,
               pointBorderColor: '#00000',
               //Data to be represented on y-axis
-              data: [4, 4, 3, 0.5]
+              data: [5, 1, 0.5]
+            }
+          ]
+        },
+        //Chart.js options that controls the appearance of the chart
+        options: {
+          tooltips: {
+            enabled: true
+          },
+          scales: {
+            yAxes: [{
+              stacked: true,
+              ticks: {
+                beginAtZero: true,
+                display: true
+              },
+              gridLines: {
+                display: false,
+              }
+            }],
+            xAxes: [ {
+              stacked: true,
+              ticks: {
+                max: 5
+              },
+              gridLines: {
+                display: true
+              }
+            }]
+          },
+          legend: {
+            display: false
+          },
+          responsive: true,
+          maintainAspectRatio: false
+        }
+      }
+    },
+    mounted () {
+      //renderChart function renders the chart with the datacollection and options object.
+      this.renderChart(this.datacollection, this.options)
+    }
+  }
+
+const containers = {
+    extends: HorizontalBar,
+    data () {
+      return {
+        datacollection: {
+          //Data to be represented on x-axis
+          labels: ['Kubernetes', 'Docker', 'Istio', 'Linkerd'],
+          datasets: [
+            {
+              backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f", "#e8c3b9"],
+              pointBackgroundColor: 'white',
+              borderWidth: 1,
+              pointBorderColor: '#00000',
+              //Data to be represented on y-axis
+              data: [4, 4, 1, 0.5]
             }
           ]
         },
@@ -106,15 +174,15 @@ const programming = {
       return {
         datacollection: {
           //Data to be represented on x-axis
-          labels: ['Python', 'Javascript', 'Java'],
+          labels: ['Python', 'Javascript', 'Java', 'Go'],
           datasets: [
             {
-              backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f"],
+              backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f", "#e8c3b9"],
               pointBackgroundColor: 'white',
               borderWidth: 1,
               pointBorderColor: '#00000',
               //Data to be represented on y-axis
-              data: [5, 4, 3]
+              data: [5, 4, 3, 0.5]
             }
           ]
         },
@@ -174,21 +242,21 @@ const programming = {
     }
   }
 
-const cicd = {
+const infrastructure = {
     extends: HorizontalBar,
     data () {
       return {
         datacollection: {
           //Data to be represented on x-axis
-          labels: ['Bamboo', 'AWS CF', 'Chef', 'Terraform'],
+          labels: ['AWS CF', 'Terraform', 'Spinnaker'],
           datasets: [
             {
-              backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f", "#e8c3b9"],
+              backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f"],
               pointBackgroundColor: 'white',
               borderWidth: 1,
               pointBorderColor: '#00000',
               //Data to be represented on y-axis
-              data: [3, 2, 2, 0.5]
+              data: [3, 2, 0.5]
             }
           ]
         },
@@ -246,13 +314,13 @@ const cicd = {
       //renderChart function renders the chart with the datacollection and options object.
       this.renderChart(this.datacollection, this.options)
     }}
-const analytics = {
+const observability = {
     extends: HorizontalBar,
     data () {
       return {
         datacollection: {
           //Data to be represented on x-axis
-          labels: ['AWS CW', 'PagerDuty', 'Datadog', 'NewRelic'],
+          labels: ['AWS CW', 'PagerDuty', 'Datadog', 'Prometheus'],
           datasets: [
             {
               backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f", "#e8c3b9"],
@@ -319,13 +387,86 @@ const analytics = {
       this.renderChart(this.datacollection, this.options)
     }}
 
-const environments = {
+const security = {
     extends: HorizontalBar,
     data () {
       return {
         datacollection: {
           //Data to be represented on x-axis
-          labels: ['Linux', 'Windows'],
+          labels: ['Crowdstrike', 'Qualys', 'Hashicorp Vault'],
+          datasets: [
+            {
+              backgroundColor: ["#3e95cd", "#8e5ea2", "#3cba9f"],
+              pointBackgroundColor: 'white',
+              borderWidth: 1,
+              pointBorderColor: '#00000',
+              //Data to be represented on y-axis
+              data: [1, 1, 1]
+            }
+          ]
+        },
+        //Chart.js options that controls the appearance of the chart
+        options: {
+          tooltips: {
+            // callbacks: {
+            //   title: function(){
+            //     return ''
+            //   },
+            //   beforeLabel: function(tooltipItem, data){
+            //     return data.datasets[tooltipItem.datasetIndex].label
+
+            //   },
+            //   label: function(tooltipItem) {
+            //     return '$' + Number(tooltipItem.yLabel)
+            //   }
+            // }
+            enabled: true
+          },
+          scales: {
+            yAxes: [{
+              stacked: true,
+              ticks: {
+                beginAtZero: true,
+                // Include a dollar sign in the ticks
+                    // callback: function(value) {
+                    //     return '$' + value;
+                    // },
+                display: true
+              },
+              gridLines: {
+                display: false,
+              }
+            }],
+            xAxes: [ {
+              stacked: true,
+              ticks: {
+                max: 5
+              },
+              gridLines: {
+                display: true
+              }
+            }]
+          },
+          legend: {
+            display: false
+          },
+          responsive: true,
+          maintainAspectRatio: false
+        }
+      }
+    },
+    mounted () {
+      //renderChart function renders the chart with the datacollection and options object.
+      this.renderChart(this.datacollection, this.options)
+    }}
+
+const cicd = {
+    extends: HorizontalBar,
+    data () {
+      return {
+        datacollection: {
+          //Data to be represented on x-axis
+          labels: ['Atlassian Bamboo', 'Github Actions'],
           datasets: [
             {
               backgroundColor: ["#3e95cd", "#8e5ea2"],
@@ -333,7 +474,7 @@ const environments = {
               borderWidth: 1,
               pointBorderColor: '#00000',
               //Data to be represented on y-axis
-              data: [6, 3]
+              data: [3, 1.5]
             }
           ]
         },
@@ -398,7 +539,7 @@ const frameworks = {
       return {
         datacollection: {
           //Data to be represented on x-axis
-          labels: ['Flask', 'VueJS', 'FastAPI', 'Spring'],
+          labels: ['VueJS', 'Flask', 'FastAPI', 'Spring'],
           datasets: [
             {
               backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f","#e8c3b9"],
@@ -406,7 +547,7 @@ const frameworks = {
               borderWidth: 1,
               pointBorderColor: '#00000',
               //Data to be represented on y-axis
-              data: [3, 2, 1, 1]
+              data: [3, 3, 1, 1]
             }
           ]
         },
